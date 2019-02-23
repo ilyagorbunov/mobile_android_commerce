01203b879f79638e4b1b359053175d6a8a5a8503
Ednoer <enurhadi24@gmail.com>
2019-02-23 14:09:19 +0700

    get data from server & show it on the emulator

--- EcommerceAppKotlin/app/src/main/java/com/packag/ecommerceappkotlin/MainActivity.kt
+++ EcommerceAppKotlin/app/src/main/java/com/packag/ecommerceappkotlin/MainActivity.kt
@@ -5,7 +5,13 @@ import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
+import javax.xml.transform.ErrorListener
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,20 +21,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnAdd.setOnClickListener(){
-            sharedP = getSharedPreferences("addData", Context.MODE_PRIVATE)
-            var myEditor = sharedP?.edit()
-            myEditor?.putString("product_name", edtProduct.text.toString())
-            myEditor?.commit()
+        btnGetData.setOnClickListener{
+            val serverURL: String = "http://172.29.87.227/ecommerc/test_file.php"
+            var requestQ: RequestQueue = Volley.newRequestQueue(this@MainActivity)
+            var stringRequest = StringRequest(Request.Method.GET, serverURL,
 
-            Toast.makeText(this@MainActivity,"The Product is saved", Toast.LENGTH_SHORT).show()
+                Response.Listener { response ->
 
+                    txtHelloWorld.text = response
+
+                }, Response.ErrorListener { error ->  
+
+                    txtHelloWorld.text = error.message
+
+                })
+            requestQ.add(stringRequest)
         }
-
-        btnGetProduct.setOnClickListener(){
-            txtGetProduct.text = sharedP?.getString("product_name", "nothing")
-
-        }
-
     }
 }
