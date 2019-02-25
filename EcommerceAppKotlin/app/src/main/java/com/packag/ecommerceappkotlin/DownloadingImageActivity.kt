fdbfb59ef8a4c53d91116b3064d7f899e4e0720b
Ednoer <enurhadi24@gmail.com>
2019-02-25 16:24:36 +0700

    Get an image from server with picasso

---
+++ EcommerceAppKotlin/app/src/main/java/com/packag/ecommerceappkotlin/DownloadingImageActivity.kt
@@ -0,0 +1,21 @@
+package com.packag.ecommerceappkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_downloading_image.*
+
+class DownloadingImageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_downloading_image)
+
+        btnDownloadImage.setOnClickListener(){
+
+            val imageURL = "http://192.168.43.103/mobile.png"
+
+            Picasso.get().load(imageURL).into(imgDownloadedImage);
+        }
+    }
+}
