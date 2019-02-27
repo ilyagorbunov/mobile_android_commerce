489b0f4b0f9fb382236b9589d69b4b05c43ecfda
Ednoer <enurhadi24@gmail.com>
2019-02-27 19:59:53 +0700

    create product view holder

--- EcommerceAppKotlin/app/src/main/java/com/packag/ecommerceappkotlin/RVAdapter.kt
+++ EcommerceAppKotlin/app/src/main/java/com/packag/ecommerceappkotlin/RVAdapter.kt
@@ -2,7 +2,11 @@ package com.packag.ecommerceappkotlin
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.TextureView
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 
 class RVAdapter(contect: Context, arrayList: ArrayList<EProduct>):
     RecyclerView.Adapter<RecyclerView.ViewHolder>(){
@@ -17,5 +21,23 @@ class RVAdapter(contect: Context, arrayList: ArrayList<EProduct>):
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
 
+    }
+
+    inner class ProductViewHolder(myView: View)
+        : RecyclerView.ViewHolder(myView){
+
+        var pIDTextView = myView.findViewById<TextView>(R.id.rv_row_pID)
+        var pNameTextView = myView.findViewById<TextView>(R.id.rv_row_pName)
+        var pPriceTextView = myView.findViewById<TextView>(R.id.rv_row_pPrice)
+        var pImageView = myView.findViewById<ImageView>(R.id.rv_row_productImage)
+
+        fun initializedUIComponents(pID: Int, pName: String, pPrice: Int, pPicture: Int){
+
+            pIDTextView.text = pID.toString()
+            pNameTextView.text = pName
+            pPriceTextView.text = pPrice.toString()
+            pImageView.setImageResource(pPicture)
+
+        }
     }
 }
