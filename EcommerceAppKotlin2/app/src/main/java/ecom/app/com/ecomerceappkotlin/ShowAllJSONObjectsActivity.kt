46b2e4efa9ab4d1eb1e5e0105b25b3d2c9af4a28
rezaerbe <rezarb88@gmail.com>
2019-02-27 20:58:55 +0700

    get data from server -> text and image -> (19)

---
+++ EcommerceAppKotlin2/app/src/main/java/ecom/app/com/ecomerceappkotlin/ShowAllJSONObjectsActivity.kt
@@ -0,0 +1,52 @@
+package ecom.app.com.ecomerceappkotlin
+
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_show_all_jsonobjects.*
+import org.json.JSONArray
+
+class ShowAllJSONObjectsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_all_jsonobjects)
+
+        txtAllProducts.setBackgroundColor(Color.BLUE)
+        txtAllProducts.setTextColor(Color.WHITE)
+
+        txtAllProducts.setOnClickListener{
+            val allProductsURL = "http://192.168.43.249/ecom/present_json_array.php"
+
+            var allProductsString: String = ""
+
+            val requestQ: RequestQueue = Volley.newRequestQueue(this@ShowAllJSONObjectsActivity)
+
+            val jsonAR = JsonArrayRequest(Request.Method.GET, allProductsURL,  null,
+                Response.Listener { response ->
+
+                    for (productIndex in 0.until( response.length())) {
+
+                        var pn = response.getJSONObject(productIndex).getString("name")
+                        var pp = response.getJSONObject(productIndex).getInt("price")
+
+                        allProductsString = allProductsString + pn + " - " + pp + "\n"
+
+                    }
+
+                    txtAllProducts.text = allProductsString
+
+                }, Response.ErrorListener { error ->
+
+                    txtAllProducts.text = error.message
+                })
+
+            requestQ.add(jsonAR)
+        }
+    }
+}
