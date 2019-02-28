f04959687a58768e791559d23fb10aa96101ea74
rezaerbe <rezarb88@gmail.com>
2019-02-28 14:31:08 +0700

    Recycler View -> (25)

---
+++ EcommerceAppKotlin2/app/src/main/java/ecom/app/com/ecomerceappkotlin/EProduct.kt
@@ -0,0 +1,17 @@
+package ecom.app.com.ecomerceappkotlin
+
+class EProduct {
+
+    var id: Int
+    var name: String
+    var price: Int
+    var productPicture: Int
+
+    constructor(i: Int, n: String, p: Int, pp: Int){
+
+        this.id = i
+        this.name = n
+        this.price = p
+        this.productPicture = pp
+    }
+}
