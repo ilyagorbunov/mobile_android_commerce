58120daecc357f9225ccb3358102747e0fdaff21
rezaerbe <rezarb88@gmail.com>
2019-03-07 10:54:20 +0700

    SignUp dan Login Selesai

--- EcommerceAppKotlin2/app/src/main/java/ecom/app/com/ecomerceappkotlin/MainActivity.kt
+++ EcommerceAppKotlin2/app/src/main/java/ecom/app/com/ecomerceappkotlin/MainActivity.kt
@@ -1,8 +1,15 @@
 package ecom.app.com.ecomerceappkotlin
 
+import android.app.AlertDialog
+import android.app.DownloadManager
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.sign_up_layout.*
 
@@ -11,6 +18,47 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        activity_main_btnLogin.setOnClickListener{
+
+            val loginURL = "http://192.168.43.249/store/login_app_user.php?email=" +
+                    activity_main_edtEmail.text.toString() + "&pass=" +
+                    activity_main_edtPassword.text.toString()
+
+            val requestQ = Volley.newRequestQueue(this@MainActivity)
+            val stringRequest = StringRequest(Request.Method.GET, loginURL, Response.Listener
+            { response ->
+
+                if (response.equals("The user does exist")) {
+
+                    Person.email = activity_main_edtEmail.text.toString()
+
+                    Toast.makeText(this@MainActivity, response, Toast.LENGTH_SHORT).show()
+
+                    val homeIntent = Intent(this@MainActivity, HomeScreen::class.java)
+                    startActivity(homeIntent)
+
+                } else {
+
+                    val dialogBuilder = AlertDialog.Builder(this)
+                    dialogBuilder.setTitle("Message")
+                    dialogBuilder.setMessage(response)
+                    dialogBuilder.create().show()
+
+                }
+
+            }, Response.ErrorListener { error ->
+
+                val dialogBuilder = AlertDialog.Builder(this)
+                dialogBuilder.setTitle("Message")
+                dialogBuilder.setMessage(error.message)
+                dialogBuilder.create().show()
+
+            })
+
+            requestQ.add(stringRequest)
+
+        }
 
         activity_main_btnSignUp.setOnClickListener{
 
--- EcommerceAppKotlin2/app/src/main/java/ecom/app/com/ecomerceappkotlin/SignUpLayout.kt
+++ EcommerceAppKotlin2/app/src/main/java/ecom/app/com/ecomerceappkotlin/SignUpLayout.kt
@@ -1,13 +1,16 @@
 package ecom.app.com.ecomerceappkotlin
 
 import android.app.VoiceInteractor
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
+import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.sign_up_layout.*
 
 class SignUpLayout : AppCompatActivity() {
@@ -39,10 +42,16 @@ class SignUpLayout : AppCompatActivity() {
 
                     } else {
 
-                        val dialogBuilder = AlertDialog.Builder(this)
-                        dialogBuilder.setTitle("Message")
-                        dialogBuilder.setMessage(response)
-                        dialogBuilder.create().show()
+//                        val dialogBuilder = AlertDialog.Builder(this)
+//                        dialogBuilder.setTitle("Message")
+//                        dialogBuilder.setMessage(response)
+//                        dialogBuilder.create().show()
+
+                        Person.email = sign_up_layout_edtEmail.text.toString()
+
+                        Toast.makeText(this@SignUpLayout, response, Toast.LENGTH_SHORT).show()
+                        val homeIntent = Intent(this@SignUpLayout, HomeScreen::class.java)
+                        startActivity(homeIntent)
 
                     }
 
@@ -67,6 +76,11 @@ class SignUpLayout : AppCompatActivity() {
             }
         }
 
+        sign_up_layout_btnLogin.setOnClickListener{
+
+            finish()
+
+        }
 
     }
 }
