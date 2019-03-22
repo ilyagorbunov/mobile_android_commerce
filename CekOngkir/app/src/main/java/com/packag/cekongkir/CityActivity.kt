b5a797ded222faca38ee29eaf1130a2f6bbe2842
Ednoer <enurhadi24@gmail.com>
2019-03-22 22:53:16 +0700

    adding city activity

--- CekOngkir/app/src/main/java/com/packag/cekongkir/CityActivity.kt
+++ CekOngkir/app/src/main/java/com/packag/cekongkir/CityActivity.kt
@@ -2,14 +2,11 @@ package com.packag.cekongkir
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class CityActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        editOrigin
+        setContentView(R.layout.activity_city)
     }
 }
