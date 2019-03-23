b4b6c0795167f846dd5a44a3427a07408960dd8e
Ednoer <enurhadi24@gmail.com>
2019-03-23 18:11:57 +0700

    get JSON Array to List Pilih Kota

--- CekOngkir/app/src/main/java/com/packag/cekongkir/data/Api.kt
+++ CekOngkir/app/src/main/java/com/packag/cekongkir/data/Api.kt
@@ -4,7 +4,7 @@ import com.packag.cekongkir.CityActivity
 
 class Api {
     companion object {
-        val BASE_URL = "https://api.rajaongkir.com/starter/city"
+        val BASE_URL = "https://api.rajaongkir.com/starter/"
         val CITY     = BASE_URL + "city"
         val COST     = BASE_URL + "cost"
         val KEY      = "e2d1393065a094cb4225c85b2bb8a4fa"
