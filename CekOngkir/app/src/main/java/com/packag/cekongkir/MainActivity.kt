e5802302e868f928d4ba1cc7cb152e795587338f
Ednoer <enurhadi24@gmail.com>
2019-03-24 21:56:42 +0700

    getJSONarray mainActivity rajaongkir -> results -> costs

--- CekOngkir/app/src/main/java/com/packag/cekongkir/MainActivity.kt
+++ CekOngkir/app/src/main/java/com/packag/cekongkir/MainActivity.kt
@@ -3,8 +3,18 @@ package com.packag.cekongkir
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
+import com.packag.cekongkir.data.Api
 import com.packag.cekongkir.data.Constant
+import kotlinx.android.synthetic.main.activity_city.*
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +31,13 @@ class MainActivity : AppCompatActivity() {
             Constant.ORIGIN = false
             startActivity(Intent(this, CityActivity::class.java))
         }
+
+        btnCekOngkir.setOnClickListener{
+            if(Constant.ORIGIN_ID.isBlank()|| Constant.DEST_ID.isBlank())
+                Toast.makeText(applicationContext, "Isi data dengan benar", Toast.LENGTH_SHORT).show()
+            else
+                getCost()
+        }
     }
 
     override fun onResume(){
@@ -32,5 +49,45 @@ class MainActivity : AppCompatActivity() {
                 editDestination.setText(Constant.DEST_NAME)
             }
         }
+    }
+
+    fun getCost(){
+        AndroidNetworking.post(Api.COST)
+            .addHeaders("key", Api.KEY)
+            .addBodyParameter("origin", Constant.ORIGIN_ID)
+            .addBodyParameter("destination", Constant.DEST_ID)
+            .addBodyParameter("weight", "1000")
+            .addBodyParameter("courier", "jne")
+            .setTag("test")
+            .setPriority(Priority.MEDIUM)
+            .build()
+            .getAsJSONObject(object : JSONObjectRequestListener {
+                override fun onResponse(response: JSONObject) {
+                    // do anything with response
+                    Log.e("_responseCost", response.toString())
+
+                    val rajaongkirObject = response.getJSONObject("rajaongkir")
+                    val resultsArray = rajaongkirObject.getJSONArray("results")
+
+                    for(i in 0 until resultsArray.length()){
+                        val resultsObject = resultsArray.getJSONObject(i)
+                        Log.e("_logCode", resultsObject["code"].toString())
+
+                        val code:String = resultsObject["code"].toString().toUpperCase()
+
+                        val costsArray = resultsObject.getJSONArray("costs")
+                        for (j in 0 until costsArray.length()){
+
+                        }
+                    }
+
+
+                }
+
+                override fun onError(error: ANError) {
+                    // handle error
+                }
+            })
+
     }
 }
