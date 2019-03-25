0ba4ffa8e4ad9761037840206da7b631931c5f59
rezaerbe <rezarb88@gmail.com>
2019-03-25 19:03:29 +0700

    Payment processing final -> (57) -> end

--- EcommerceAppKotlin2/app/src/main/java/ecom/app/com/ecomerceappkotlin/FinalizeShoppingActivity.kt
+++ EcommerceAppKotlin2/app/src/main/java/ecom/app/com/ecomerceappkotlin/FinalizeShoppingActivity.kt
@@ -2,11 +2,31 @@ package ecom.app.com.ecomerceappkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.RequestFuture
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_finalize_shopping.*
 
 class FinalizeShoppingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finalize_shopping)
+
+        var calculateTotalPriceUrl = "http://192.168.43.249/store/calculate_total_price.php?invoice_num=${intent.getStringExtra("LATEST_INVOICE_NUMBER")}"
+        var requestQ = Volley.newRequestQueue(this@FinalizeShoppingActivity)
+        var stringRequest = StringRequest(Request.Method.GET, calculateTotalPriceUrl, Response.Listener { response ->  
+            
+            btnPaymentProcessing.text = "Bayar Rp.$response di tempat"
+            
+        }, Response.ErrorListener { error ->
+
+
+        })
+
+        requestQ.add(stringRequest)
+
     }
 }
