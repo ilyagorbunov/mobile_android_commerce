4d8f294990b363fd788bf598b81627744021776a
rezaerbe <rezarb88@gmail.com>
2019-03-25 15:12:20 +0700

    add to cart + temporary order -> (48)

---
+++ EcommerceAppKotlin2/app/src/main/java/ecom/app/com/ecomerceappkotlin/AmountFragment.kt
@@ -0,0 +1,52 @@
+package ecom.app.com.ecomerceappkotlin
+
+
+import android.app.DialogFragment
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
+class AmountFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        var fragmentView =  inflater.inflate(R.layout.fragment_amount, container, false)
+
+        var edtEnterAmount = fragmentView.findViewById<EditText>(R.id.edtEnterAmount)
+        var btnAddToCart = fragmentView.findViewById<ImageButton>(R.id.btnAddToCart)
+
+        btnAddToCart.setOnClickListener {
+
+            var ptoUrl = "http://192.168.43.249/store/insert_temporary_order.php?email=${Person.email}&product_id=${Person.addToCartProductID}&amount=${edtEnterAmount.text.toString()}"
+            var requestQ = Volley.newRequestQueue(activity)
+            var stringRequest = StringRequest(Request.Method.GET, ptoUrl, Response.Listener { response ->
+                
+                var intent = Intent(activity, CartProductsActivity::class.java)
+                startActivity(intent)
+            }, Response.ErrorListener { error ->
+
+
+            })
+
+            requestQ.add(stringRequest)
+
+        }
+
+        return fragmentView
+    }
+
+
+}
