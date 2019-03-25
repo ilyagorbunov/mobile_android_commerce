43157d5ae9ebf39b5c86d622f39077409c129b7c
rezaerbe <rezarb88@gmail.com>
2019-03-25 16:51:46 +0700

    temporary order -> (50)

--- EcommerceAppKotlin2/app/src/main/java/ecom/app/com/ecomerceappkotlin/CartProductsActivity.kt
+++ EcommerceAppKotlin2/app/src/main/java/ecom/app/com/ecomerceappkotlin/CartProductsActivity.kt
@@ -2,11 +2,44 @@ package ecom.app.com.ecomerceappkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_cart_products.*
 
 class CartProductsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart_products)
+
+        var cartProductsUrl = "http://192.168.43.249/store/fetch_temporary_order.php?email=${Person.email}"
+        var cartProductsList = ArrayList<String>()
+        var requestQ = Volley.newRequestQueue(this@CartProductsActivity)
+        var jsonAR  = JsonArrayRequest(Request.Method.GET, cartProductsUrl, null, Response.Listener { response ->
+
+            for (joIndex in 0.until(response.length())){
+
+                cartProductsList.add("${response.getJSONObject(joIndex).getInt("id")} " +
+                        "\n ${response.getJSONObject(joIndex).getString("name")} " +
+                        "\n ${response.getJSONObject(joIndex).getInt("price")} " +
+                        "\n ${response.getJSONObject(joIndex).getString("email")} " +
+                        "\n ${response.getJSONObject(joIndex).getInt("amount")}")
+
+            }
+
+            var cartProductsAdapter = ArrayAdapter(this@CartProductsActivity, android.R.layout.simple_list_item_1, cartProductsList)
+            cartProductsListView.adapter = cartProductsAdapter
+
+        }, Response.ErrorListener { error ->
+
+
+
+        })
+
+        requestQ.add(jsonAR)
+
     }
 }
