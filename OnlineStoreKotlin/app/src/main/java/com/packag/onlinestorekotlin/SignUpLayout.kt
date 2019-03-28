92bbd494e6c6dbbc7159fff11925f5e50e5760e9
Ednoer <enurhadi24@gmail.com>
2019-03-28 21:18:36 +0700

    password mismatch

--- OnlineStoreKotlin/app/src/main/java/com/packag/onlinestorekotlin/SignUpLayout.kt
+++ OnlineStoreKotlin/app/src/main/java/com/packag/onlinestorekotlin/SignUpLayout.kt
@@ -3,6 +3,9 @@ package com.packag.onlinestorekotlin
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_sign_up_layout.*
 
 class SignUpLayout : AppCompatActivity() {
 
@@ -13,5 +16,21 @@ class SignUpLayout : AppCompatActivity() {
         supportActionBar!!.setTitle("Sign Up")
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
+        btnSignUp.setOnClickListener() {
+
+            if (edtSignUpPassword.text.toString().equals(
+                    edtSignUpConfirmPassword.text.toString())){
+
+                // registration
+
+            } else {
+                val dialogBuilder = AlertDialog.Builder(this)
+                dialogBuilder.setTitle("Message")
+                dialogBuilder.setMessage("Password Mismatch")
+                dialogBuilder.create().show()
+
+            }
+
+        }
     }
 }
