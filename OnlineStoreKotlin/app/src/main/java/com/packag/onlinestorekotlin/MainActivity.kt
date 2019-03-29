9fa46ddbc209d7df6c4b643c39048229bb4bcee7
Ednoer <enurhadi24@gmail.com>
2019-03-29 21:56:18 +0700

    Login Kt

--- OnlineStoreKotlin/app/src/main/java/com/packag/onlinestorekotlin/MainActivity.kt
+++ OnlineStoreKotlin/app/src/main/java/com/packag/onlinestorekotlin/MainActivity.kt
@@ -4,7 +4,13 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.ActionBar
+import android.support.v7.app.AlertDialog
 import android.view.Window
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,14 +18,50 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar?.hide()
 
+        btnLogin.setOnClickListener{
 
-        btnLoginSignUp.setOnClickListener{
-            var signUpIntent = Intent(this@MainActivity, SignUpLayout::class.java)
-            startActivity(signUpIntent)
+            val loginURL = "http://10.5.28.122/Ecommerc/login_app_user.php?email=" +
+                    edtLoginEmail.text.toString() + "&pass=" +
+                    edtLoginPassword.text.toString()
+
+            val requestQ = Volley.newRequestQueue(this@MainActivity)
+            val stringRequest = StringRequest(
+                Request.Method.GET, loginURL, Response.Listener
+            { response ->
+
+                if (response.equals("The user does exist")) {
+
+                    Toast.makeText(this@MainActivity, response, Toast.LENGTH_SHORT).show()
+
+                } else {
+
+                    val dialogBuilder = AlertDialog.Builder(this)
+                    dialogBuilder.setTitle("Message")
+                    dialogBuilder.setMessage(response)
+                    dialogBuilder.create().show()
+
+                }
+
+            }, Response.ErrorListener { error ->
+
+                val dialogBuilder = AlertDialog.Builder(this)
+                dialogBuilder.setTitle("Message")
+                dialogBuilder.setMessage(error.message)
+                dialogBuilder.create().show()
+
+            })
+
+            requestQ.add(stringRequest)
+
         }
 
+        btnLoginSignUp.setOnClickListener{
+
+            var signUpIntent = Intent(this@MainActivity, SignUpLayout::class.java)
+            startActivity(signUpIntent)
+
+        }
     }
 
 }
