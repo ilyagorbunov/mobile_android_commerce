8e838ca9c76f4c5898aca8be55e6890ee50bf257
Ednoer <enurhadi24@gmail.com>
2019-04-03 07:25:44 +0700

    Fetch E-Products 2

--- OnlineStoreKotlin/app/src/main/java/com/packag/onlinestorekotlin/EProductAdapter.kt
+++ OnlineStoreKotlin/app/src/main/java/com/packag/onlinestorekotlin/EProductAdapter.kt
@@ -1,4 +1,22 @@
 package com.packag.onlinestorekotlin
 
-class EProductAdapter {
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+
+class EProductAdapter(var context: Context,
+                      var arrayList: ArrayList<EProduct>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
