18a9b5ec434c6f1eaca2a718cc65edf48e618bd5
Ednoer <enurhadi24@gmail.com>
2019-04-03 12:06:00 +0700

    initialize row ui commponents

--- OnlineStoreKotlin/app/src/main/java/com/packag/onlinestorekotlin/EProductAdapter.kt
+++ OnlineStoreKotlin/app/src/main/java/com/packag/onlinestorekotlin/EProductAdapter.kt
@@ -2,7 +2,10 @@ package com.packag.onlinestorekotlin
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.e_product_row.view.*
 
 class EProductAdapter(var context: Context,
                       var arrayList: ArrayList<EProduct>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -18,5 +21,23 @@ class EProductAdapter(var context: Context,
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class ProductViewHolder(pView: View) : RecyclerView.ViewHolder(pView){
+
+        fun initializeRowUIComponents(id: Int, name: String, price: Int, picName: String){
+
+            itemView.txtId.text = "Id : " + id.toString()
+            itemView.txtName.text = "Name : " + name
+            itemView.txtPrice.text = "Price : " + price.toString()
+
+            var picUrl = "http://192.168.43.103/Ecommerc/osimages/"
+            picUrl = picUrl.replace(" ", "%20")
+            Picasso.get().load(picUrl + picName).into(itemView.imgProduct)
+
+
+        }
+
+
     }
 }
