a59311cb1648a2159611f26b223c65c6208d046b
Ednoer <enurhadi24@gmail.com>
2019-04-17 05:58:57 +0700

    facebook account kit

--- AndroidEcommerce/app/src/main/java/com/packag/androidecommerce/MainActivity.java
+++ AndroidEcommerce/app/src/main/java/com/packag/androidecommerce/MainActivity.java
@@ -6,24 +6,39 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.os.Message;
+import android.support.annotation.CheckResult;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Base64;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
 import com.facebook.accountkit.AccountKitLoginResult;
 import com.facebook.accountkit.ui.AccountKitActivity;
 import com.facebook.accountkit.ui.AccountKitConfiguration;
 import com.facebook.accountkit.ui.LoginType;
+import com.packag.androidecommerce.Model.CheckUserResponse;
+import com.packag.androidecommerce.Retrofit.IDrinkShopAPI;
+import com.packag.androidecommerce.Utils.Common;
+import com.rengwuxian.materialedittext.MaterialEditText;
+import com.szagurskii.patternedtextwatcher.PatternedTextWatcher;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 import dmax.dialog.SpotsDialog;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
  public class MainActivity extends AppCompatActivity {
@@ -31,10 +46,14 @@ import dmax.dialog.SpotsDialog;
      private static final int REQUEST_CODE = 1000;
      Button btn_continue;
 
+     IDrinkShopAPI mService;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mService = Common.getAPI();
 
         btn_continue = (Button)findViewById(R.id.btn_continue);
         btn_continue.setOnClickListener(new View.OnClickListener(){
@@ -75,15 +94,125 @@ import dmax.dialog.SpotsDialog;
              {
                  if(result.getAccessToken() != null)
                  {
-                     AlertDialog alertDialog = new SpotsDialog(MainActivity.this);
+                     final AlertDialog alertDialog = new SpotsDialog(MainActivity.this);
                      alertDialog.show();
                      alertDialog.setMessage("Please waiting...");
 
                      //Get User phone and check exist on serve
+                     AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+                         @Override
+                         public void onSuccess(final Account account) {
+
+                             mService.checkUserExists(account.getPhoneNumber().toString())
+                                     .enqueue(new Callback<CheckUserResponse>() {
+                                         @Override
+                                         public void onResponse(Call<CheckUserResponse> call, Response<CheckUserResponse> response) {
+                                             CheckUserResponse userResponse = response.body();
+                                             if(userResponse.isExists())
+                                             {
+                                                 // if user already exists, just start new Activity
+                                                 alertDialog.dismiss();
+                                             }
+                                             else
+                                             {
+                                                 //need register
+                                                 alertDialog.dismiss();
+
+                                                 showRegisterDialog(account.getPhoneNumber().toString());
+                                             }
+
+                                         }
+
+                                         @Override
+                                         public void onFailure(Call<CheckUserResponse> call, Throwable t) {
+
+                                         }
+                                     });
+
+                         }
+
+                         @Override
+                         public void onError(AccountKitError accountKitError) {
+                             Log.d("ERROR", accountKitError.getErrorType().getMessage());
+                         }
+                     });
 
                  }
              }
          }
+     }
+
+
+     private void showRegisterDialog(final String phone) {
+
+         final android.support.v7.app.AlertDialog.Builder alertDialog = new android.support.v7.app.AlertDialog.Builder(MainActivity.this);
+         alertDialog.setTitle("REGISTER");
+
+         LayoutInflater inflater = this.getLayoutInflater();
+         final View register_layout = inflater.inflate(R.layout.register_layout, null);
+
+         final MaterialEditText edt_name = (MaterialEditText)register_layout.findViewById(R.id.edt_name);
+         final MaterialEditText edt_address = (MaterialEditText)register_layout.findViewById(R.id.edt_address);
+         final MaterialEditText edt_birthdate = (MaterialEditText)register_layout.findViewById(R.id.edt_birthdate);
+
+         Button btn_register = (Button)register_layout.findViewById(R.id.btn_register);
+
+         edt_birthdate.addTextChangedListener(new PatternedTextWatcher("####-##-##"));
+
+         //event
+         btn_register.setOnClickListener(new View.OnClickListener(){
+             @Override
+             public void onClick(View v){
+
+                 //close dialog
+                 alertDialog.create().dismiss();
+
+                 if(TextUtils.isEmpty(edt_address.getText().toString()))
+                 {
+                     Toast.makeText(MainActivity.this,"Please enter your address", Toast.LENGTH_SHORT).show();
+                     return;
+                 }
+                 if(TextUtils.isEmpty(edt_name.getText().toString()))
+                 {
+                     Toast.makeText(MainActivity.this,"Please enter your name", Toast.LENGTH_SHORT).show();
+                     return;
+                 }
+                 if(TextUtils.isEmpty(edt_birthdate.getText().toString()))
+                 {
+                     Toast.makeText(MainActivity.this,"Please enter your birthdate", Toast.LENGTH_SHORT).show();
+                     return;
+                 }
+
+                 final AlertDialog watingDialog = new SpotsDialog(MainActivity.this);
+                 watingDialog.show();
+                 watingDialog.setMessage("Please waiting...");
+
+                 mService.registerNewUser(phone,
+                         edt_name.getText().toString(),
+                         edt_address.getText().toString(),
+                         edt_birthdate.getText().toString())
+                         .enqueue(new Callback<User>() {
+                             @Override
+                             public void onResponse(Call<User> call, Response<User> response) {
+                                 watingDialog.dismiss();
+                                 User user = response.body();
+
+                                 if(TextUtils.isEmpty(user.getError_msg()))
+                                 {
+                                     Toast.makeText(MainActivity.this, "User register successfully", Toast.LENGTH_SHORT).show();
+                                 }
+
+                             }
+
+                             @Override
+                             public void onFailure(Call<User> call, Throwable t) {
+                                    watingDialog.dismiss();
+                             }
+                         });
+             }
+         });
+         alertDialog.setView(register_layout);
+         alertDialog.show();
      }
 
      private void printKeyHash() {
